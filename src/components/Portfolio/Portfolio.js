fe741380df1235cf9f9019a182ee36574cc0ffd6
Vishakha Sahu <vishakhasahu023@gmail.com>
2022-11-19 15:26:13 +0530

    v - 2 card component added for Portfolio

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,9 +1,7 @@
 import React from 'react';
+import PortfolioCard from './PortfolioCard';
+import PortfolioData from './PortfolioData';
 import './Portfolio.css';
-import doIt from '../images/portfolio/DOIT.png'
-import quote from '../images/portfolio/quote.png';
-import social from '../images/portfolio/social.png';
-import song from '../images/portfolio/song.png';
 
 const Portfolio = () => {
   return (
@@ -20,68 +18,16 @@ const Portfolio = () => {
       </div>
 
       <div className="container-fluid">
-
         <div className="row">
-
           <div className="post_gallery owl-theme">
             {/* owl-carousel */}
-            <div className="item">
-              <div className="portfolio-item position-relative">
-                <img src={doIt} className="img-fluid" alt="do it" width='450px' />
-
-                <div className="portoflio-item-overlay">
-                  <a href="#"><i className="ti-plus"></i></a>
-                </div>
-              </div>
-              <div className="text mt-3">
-                <h4 className="mb-1 text-capitalize">Web development</h4>
-                <p className="text-uppercase letter-spacing text-sm">wordpress</p>
-              </div>
-            </div>
-            <div className="item">
-              <div className="portfolio-item position-relative">
-                <img src={quote} alt="quote" className="img-fluid" width='450px' />
-
-                <div className="portoflio-item-overlay">
-                  <a href="portfolio-single.html"><i className="ti-plus"></i></a>
-                </div>
-              </div>
-              <div className="text mt-3">
-                <h4 className="mb-1 text-capitalize">Web development</h4>
-                <p className="text-uppercase letter-spacing text-sm">wordpress</p>
-              </div>
-            </div>
-            <div className="item">
-              <div className="portfolio-item position-relative">
-                <img src={social} alt="social" className="img-fluid" width='450px' />
-
-                <div className="portoflio-item-overlay">
-                  <a href="portfolio-single.html"><i className="ti-plus"></i></a>
-                </div>
-              </div>
-              <div className="text mt-3">
-                <h4 className="mb-1 text-capitalize">Web development</h4>
-                <p className="text-uppercase letter-spacing text-sm">wordpress</p>
-              </div>
-            </div>
-            <div className="item">
-              <div className="portfolio-item position-relative">
-                <img src={song} alt="song" className="img-fluid" width='450px' />
-
-                <div className="portoflio-item-overlay">
-                  <a href="portfolio-single.html"><i className="ti-plus"></i></a>
-                </div>
-              </div>
-              <div className="text mt-3">
-                <h4 className="mb-1 text-capitalize">Web development</h4>
-                <p className="text-uppercase letter-spacing text-sm">wordpress</p>
-              </div>
-            </div>
-
+            {
+              PortfolioData.map((value, index) => {
+                return <PortfolioCard key={index} img={value.image} title={value.title} text={value.text} />
+              })
+            }
           </div>
         </div>
-
-
       </div>
     </section>
   )
---
+++ src/components/Portfolio/PortfolioData.js
@@ -0,0 +1,29 @@
+import doIt from '../images/portfolio/DOIT.png';
+import social from '../images/portfolio/social.png';
+import quote from '../images/portfolio/quote.png';
+import song from '../images/portfolio/song.png';
+
+const PortfolioData = [
+    {
+        image: song,
+        title: "Listen songs",
+        text: "Listen to Top Music around you Explore top Artists and Trending songs."
+    },
+    {
+        image: social,
+        title: "Get Social",
+        text: "A social platform that helps you share your art and work with people around you."
+    },
+    {
+        image: doIt,
+        title: "DO.IT",
+        text: "Multi functional Todo App, helps you be ahead of the day mold your day to be super productive"
+    },
+    {
+        image: quote,
+        title: "My Quotes",
+        text: "Share Quotes"
+    },
+]
+
+export default PortfolioData;
