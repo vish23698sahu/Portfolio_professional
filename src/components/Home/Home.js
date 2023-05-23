7dfbe1d0e00a38f4b1cb9391ec50d4ac1a07cad3
Vishakha Sahu <vishakhasahu023@gmail.com>
2023-05-23 09:25:09 +0530

    v - 36 Home updated

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import profilePic from '../images/profilephoto.png';
-import './Home.css';
 import About from '../About/About';
+import Contact from '../Contact/Contact';
 import Expertise from '../Expertise/Expertise';
 import Portfolio from '../Portfolio/Portfolio';
-import Contact from '../Contact/Contact';
+import profilePic from '../images/profilephoto.png';
+import './Home.css';
 
 const Home = () => {
   return (
     <div>
-
       <section id='home' className="slider py-7">
         <div className="container">
           <div className="row align-items-center">
@@ -22,13 +21,14 @@ const Home = () => {
             <div className="col-lg-6 col-12 col-md-7">
               <div className="ml-5 position-relative mt-5 mt-lg-0">
                 <span className="head-trans">Vishakha</span>
-                <h1 className="font-weight-normal text-color text-md"><i className="ti-minus mr-2"></i>Web Developer</h1>
+                <h1 className="font-weight-normal text-color text-md"><i className="ti-minus mr-2"></i>React Developer</h1>
                 <h2 className="mt-3 text-lg mb-3 text-capitalize">Vishakha Sahu.</h2>
                 <p className="animated fadeInUp lead mt-4 mb-5 text-white-50 lh-35">
-                  A Front End Web Developer, Highly engaged in building clean front end code.<br />
-                  Well organized person, problem solver, with love for books, outdoor games, and Yoga.
+                  Passionate Full Stack Developer, specialized in crafting clean, user-friendly interfaces.
+                  A highly organized problem-solver, dedicated to creating efficient code.<br />
+                  Beyond my technical expertise, I am an avid reader, a lover of outdoor games, and a Yoga enthusiast.
                 </p>
-                <a href="#about" className="btn-solid-border btn-me btn mt-5 ">About Me</a>
+                <a href="#about" className="btn-solid-border btn-me btn mt-5 button_about">About Me</a>
               </div>
             </div>
           </div>
@@ -51,4 +51,4 @@ const Home = () => {
   )
 }
 
-export default Home
+export default Home;
